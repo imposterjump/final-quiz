ba7fb2941776708ff314db9c5cd7755653517bba
imposterjump <125587220+imposterjump@users.noreply.github.com>
2023-06-01 13:28:24 +0300

    .

--- index.js
+++ index.js
@@ -1,49 +1,34 @@
-import HttpError from "http-errors";
 import express from "express";
 import path from "path";
+import cookieParser from "cookie-parser";
 import logger from "morgan";
 import { fileURLToPath } from "url";
 import fileUpload from "express-fileupload";
 
+// express app
+const app = express();
+
+const dbURI = 'mongodb+srv://imposterjump2:25102002@cluster0.z6cgz77.mongodb.net/jump?retryWrites=true&w=majority';;
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => app.listen(8080))
+    .catch(err => console.log(err));
+
+app.use(express.urlencoded({ extended: true }));
+
+// // default options
+app.use(fileUpload());
+app.use(express.static('public'));
+app.use(session({ secret: 'Your_Secret_Key' }))
+    // register view engine
+app.set('view engine', 'ejs');
 
 
 
-
-
-export const __filename = fileURLToPath(
-    import.meta.url);
-export const __dirname = path.dirname(__filename);
-console.log(`Project Root dir : ${__dirname}`);
-
-
-
-
-// 
-let index = express();
-
-
-// setting where i am taking the views pages and setting views engine to ejs  
-index.set('views', path.join(__dirname, "views"));
-index.set("view engine", "ejs");
-
-//
-index.use(logger("common"));
-index.use(express.json());
-index.use(fileUpload());
-//app.use(session({ secret: 'Your_Secret_Key' }));
-
-index.use(express.urlencoded({ extended: true }));
-
-//setup cookie parser middleware
-index.use(cookieParser());
-
-//setup static folder for serving static files in Express
-index.use(express.static(path.join(__dirname, 'public')));
-
-
-// routes setup (pls focus team while filling this )
-//home and about pages
-
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404', { user: (req.session.user === undefined ? "" : req.session.user) });
+});
 
 
 
